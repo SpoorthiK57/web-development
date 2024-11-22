6d3a06e8f0b71c7d0fda2321210258afafe8bdee
Spoorthi.Kamidi <Spoorthi.Kamidi@ever.ag>
2024-11-21 18:39:45 -0600

    Added js file

---
+++ website/server.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("1");
+let a=10;
+function home(){
+ document.getElementById("name2").innerText ="login successful";
+
+    console.log("hi, Thank you for reaching out");
+}
+console.log(a);
+//home();
+console.log(a);
+for( var i=0;i<=100;i++){
+    console.log(i);
+
+}
